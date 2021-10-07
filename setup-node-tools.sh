fcedd77e4ef5c904edf09a37912f03a909b0a673
Fábio Colombo <79655645+fbcolombo@users.noreply.github.com>
2021-10-07 15:00:42 -0300

    Update setup-node-tools.sh

--- setup-node-tools.sh
+++ setup-node-tools.sh
@@ -1,2 +1,2 @@
-echo "> Cloning node-tools repo at git@bitbucket.org:forhumans/node-tools.git"
-git clone git@bitbucket.org:forhumans/node-tools.git ./node-tools
+echo "> Cloning node-tools repo at git@bitbucket.org:forhumans/node-tools.git";
+git clone git@bitbucket.org:forhumans/node-tools.git ./node-tools;
