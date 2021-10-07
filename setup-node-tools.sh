5dabb461777e42af7aa87023c5e5429a270e5849
Fábio Colombo <79655645+fbcolombo@users.noreply.github.com>
2021-10-07 16:29:57 -0300

    Update setup-node-tools.sh

--- setup-node-tools.sh
+++ setup-node-tools.sh
@@ -1,2 +1,7 @@
-echo "> Cloning node-tools repo at git@bitbucket.org:forhumans/node-tools.git";
-git clone git@bitbucket.org:forhumans/node-tools.git ./node-tools;
+echo "> Cloning node-tools repo at git@bitbucket.org:forhumans/node-tools.git"
+git clone git@bitbucket.org:forhumans/node-tools.git ./node-tools
+echo "> Installing dependencies for node-tools"
+cd ./node-tools
+yarn instal
+echo "> Fixing permissions"
+chmod u+x ./bin/node-tools
